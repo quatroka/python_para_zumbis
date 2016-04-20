b4fec4ada6ee646b3d77fe47eeda1cd829aa0ac4
quatroka <monteiro.leonardosantos@gmail.com>
2016-04-20 08:57:49 +0000

    Lista 1 -> Questão 5 -> Concluido

---
+++ lista_de_exercicios_1/questao05.py
@@ -0,0 +1,9 @@
+#Questao 5
+
+preco = float(input("Digite o valor da mercadoria: R$"))
+porcentagem = float(input("Digite a porcentagem do desconto:"))
+desconto = porcentagem/100*preco
+total = preco-desconto
+print("O valor do desconto e: R$ %.2f" %desconto)
+print("Valor com desconto: R$ %.2f" %total)
+
