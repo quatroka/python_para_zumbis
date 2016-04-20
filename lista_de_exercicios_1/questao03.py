ff2e51dd280d0e897737af17acac2ab0a2dff868
quatroka <monteiro.leonardosantos@gmail.com>
2016-04-20 08:32:32 +0000

    Lista 1 -> Questão 3 -> Concluido

---
+++ lista_de_exercicios_1/questao03.py
@@ -0,0 +1,7 @@
+#Questao 3
+
+dias = int(input("Digite os dias:"))
+horas = int(input("Digite as horas:"))
+minutos = int(input("Digite os minutos:"))
+segundos = int(input("Digite os segundos:"))
+print("O total em segundos e:", ((dias*86400)+(horas*3600)+(minutos*60)+segundos))
