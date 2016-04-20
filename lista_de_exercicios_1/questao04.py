63368e4d29e02ef40e48bcf9167809fcb1105d75
quatroka <monteiro.leonardosantos@gmail.com>
2016-04-20 08:58:36 +0000

    Lista 1 -> Questão 4 -> Reparo

--- lista_de_exercicios_1/questao04.py
+++ lista_de_exercicios_1/questao04.py
@@ -3,7 +3,7 @@
 salario = float(input("Digite o salario: R$"))
 porcentagem = float(input("Digite a porcentagem de aumento:"))
 aumento = salario*(porcentagem/100)
-novoSalario = salario*(porcentagem/100)+salario
+novoSalario = salario+aumento
 print("O aumento será de: R$ %.2f" %aumento)
 print("O novo salario e: R$ %.2f" %novoSalario)
 
